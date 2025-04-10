7885b6aad5bc37a21aba5b608d8026a08f6599c0
ivan <ivanav457@gmail.com>
2025-04-10 22:17:51 +0500

    .

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,57 @@
 function getArrayParams(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  }
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+  let avg = Number(
+    (arr.reduce((acc, cur) => acc + cur, sum) / arr.length).toFixed(2)
+  );
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  }
+  return arr.reduce((acc, cur) => acc + cur, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  }
+  return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  }
+  return (
+    arr.reduce((acc, cur) => (cur % 2 === 0 ? acc + cur : acc), 0) -
+    arr.reduce((acc, cur) => (cur % 2 !== 0 ? acc + cur : acc), 0)
+  );
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (!arr.length) {
+    return 0;
+  }
+  let count = 0;
+  return (
+    arr.reduce((acc, cur) => {
+      if (cur % 2 === 0) {
+        count++;
+        return acc + cur;
+      } else {
+        return acc;
+      }
+    }, 0) / count
+  );
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  return Math.max(...arrOfArr.map((value) => func(...value)));
 }
