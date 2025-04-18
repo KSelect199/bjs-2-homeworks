01468a1e26e4e4ffbdfe0b51e2b8140032d5d0c5
ivan <ivanav457@gmail.com>
2025-04-18 19:20:27 +0500

    5.classes

---
+++ 5.classes/task.js
@@ -0,0 +1,97 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set state(number) {
+    if (number < 0) {
+      this._state = 0;
+    } else if (number > 100) {
+      this._state = 100;
+    } else {
+      this._state = number;
+    }
+  }
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    let findBook = this.books.filter((obj) => obj[type] === value)[0];
+    if (findBook) {
+      return findBook;
+    }
+    return null;
+  }
+
+  giveBookByName(bookName) {
+    let deleteBook = null;
+    this.books = this.books.filter((obj) => {
+      if (obj.name !== bookName) {
+        return true;
+      } else {
+        deleteBook = obj;
+        return false;
+      }
+    });
+    if (deleteBook) {
+      return deleteBook;
+    }
+    return null;
+  }
+}
